87902830747be1c366e1f2f49de7e8ad3642eacf
Muhammadjasur <jasurmuhammadjonov1122@gmail.com>
2026-08-30 16:24:07 +0900

    Update project

--- train.js
+++ train.js
@@ -34,33 +34,49 @@
 // console.log("passed here 1");
 
 
-console.log("Jack Ma maslahatlari");
+console.log("=========================================================================");
 
-const list = [
-  "yaxshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "ozingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
 
-  async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("insert a number");
-   else if (a <= 20) return list[0];
-   else if (a > 20 && a <= 30) return list[1];
-   else if (a > 30 && a <= 40) return list[2];
-   else if (a > 40 && a <= 50) return list[3];
-   else if (a > 50 && a <= 60) return list[4];
-   else {
-    return new Promise((resolve, reject) =>
-    {
-        setInterval(() => {
-            resolve(list[5]);
-        }, 1000);
-    });
-   }
-  }
+
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//   "yaxshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "ozingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+//   async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("insert a number");
+//    else if (a <= 20) return list[0];
+//    else if (a > 20 && a <= 30) return list[1];
+//    else if (a > 30 && a <= 40) return list[2];
+//    else if (a > 40 && a <= 50) return list[3];
+//    else if (a > 50 && a <= 60) return list[4];
+//    else {
+//     return new Promise((resolve, reject) =>
+//     {
+//         setInterval(() => {
+//             resolve(list[5]);
+//         }, 1000);
+//     });
+//    }
+//   }
+
+//   // asyn/await
+//     async function run() {
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(42);
+//     console.log(javob);
+//   }
+//   run();
+
 
    // setTimeout(function() {
     //       callback(null, list[5]);
@@ -77,13 +93,32 @@ const list = [
 //     });
 //     console.log("passed here 1");
 
-   // asyn/await
-    async function run() {
-    let javob = await maslahatBering(20);
-    console.log(javob);
-    javob = await maslahatBering(31);
-    console.log(javob);
-    javob = await maslahatBering(42);
-    console.log(javob);
-  }
-  run();
+   
+
+
+
+
+
+
+
+  console.log("===========================================================================================");
+
+
+
+function countVowels(word) {
+    let count = 0;
+    let vowels = "aeiou"; 
+
+    for (let i = 0; i < word.length; i++) {
+        let letter = word[i].toLowerCase(); 
+        if (vowels.includes(letter)) {
+            count = count + 1;
+        }
+    }
+
+    return count;
+}
+
+
+console.log(countVowels("engineer")); 
+console.log(countVowels("Tashkent")); 
